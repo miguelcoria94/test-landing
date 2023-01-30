fec43b009b6b34b087c7291ba32903f22fb0fb88
miguelcoria94 <miguelcoria94@gmail.com>
2023-01-30 16:10:56 -0600

    added job thing

--- src/partials/Career.vue
+++ src/partials/Career.vue
@@ -71,6 +71,69 @@
               </div>
             </div>
           </div>
+          <div
+            class="py-3"
+            data-aos="fade-up"
+            data-aos-anchor="[data-aos-id-career]"
+          >
+            <div class="pb-6 border-b border-gray-800">
+              <div class="mb-2">
+                <a
+                  class="flex justify-between items-center text-xl font-bold text-green-500 hover:text-green-400 transition duration-150 ease-in-out"
+                  href="https://forms.gle/4Rbb3xHY9euuAiPd8" target="_blank"
+                >
+                  <span>Business Development Intern</span>
+                  <svg
+                    class="stroke-current shrink-0 ml-2"
+                    width="24"
+                    height="24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g stroke-width="2" fill="none">
+                      <path d="M1 12h20" />
+                      <path stroke-linecap="square" d="M14 5l7 7-7 7" />
+                    </g>
+                  </svg>
+                </a>
+              </div>
+              <div class="text-lg text-gray-400 mb-3">
+               The ideal candidate is a self-starter with a strong work ethic and a desire to succeed in a commission-only role. This is an excellent opportunity for a job that can earn a full salary based on performance. Our ideal candidate may have a proven track record of success in creating partnerships and the ability to build and maintain relationships with clients. Experience in the mental health industry is also a plus, but not required.
+              </div>
+              <div class="text-gray-400 flex flex-wrap items-center">
+                <div class="inline-flex items-center">
+                  <svg
+                    class="mr-2 shrink-0"
+                    vidth="14"
+                    height="17"
+                    viewBox="0 0 14 17"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      class="fill-current text-green-500"
+                      d="M7 0C3.1 0 0 3.1 0 7c0 1.9.7 3.7 2.1 5 .1.1 4.1 3.7 4.2 3.8.4.3 1 .3 1.3 0 .1-.1 4.2-3.7 4.2-3.8 1.4-1.3 2.1-3.1 2.1-5 .1-3.9-3-7-6.9-7zm0 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z"
+                    />
+                  </svg>
+                  <span>Anywhere (Remote)</span>
+                </div>
+                <!-- <span class="text-gray-700 mx-3"> - </span> -->
+                <!-- <div class="inline-flex items-center">
+                  <svg
+                    class="mr-2 shrink-0"
+                    vidth="14"
+                    height="16"
+                    viewBox="0 0 14 16"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      class="fill-current text-gray-300"
+                      d="M9.5 3L11 0H3l1.5 3zM10 5H4c-2 1.458-4 4.235-4 6.4C0 12.573.5 16 7 16c6.495 0 7-3.431 7-4.6 0-2.165-2-4.942-4-6.4z"
+                    />
+                  </svg>
+                  <span>$70k – $120k</span>
+                </div> -->
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
