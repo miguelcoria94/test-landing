07339026bb0ef9f40c0115da264d402170c3903b
miguelcoria94 <miguelcoria94@gmail.com>
2023-02-01 17:28:18 -0600

    bug fix

--- src/pages/Admin.vue
+++ src/pages/Admin.vue
@@ -3,34 +3,8 @@
 </template>
 
 <script>
-import { Head } from "@vueuse/head";
-import Header from "./../partials/Header.vue";
-import PageIllustration from "../partials/PageIllustration.vue";
-import HeroAbout from "../partials/HeroAbout.vue";
-import TeamImages from "../partials/TeamImages.vue";
-import Timeline from "../partials/Timeline.vue";
-import Team from "../partials/Team.vue";
-import TestimonialsCarousel from "../partials/TestimonialsCarousel.vue";
-import Career from "../partials/Career.vue";
-import Clients from "../partials/Clients.vue";
-import Newsletter from "../partials/Newsletter.vue";
-import Footer from "./../partials/Footer.vue";
 
 export default {
   name: "Admin",
-  components: {
-    Head,
-    Header,
-    PageIllustration,
-    HeroAbout,
-    TeamImages,
-    Timeline,
-    Team,
-    TestimonialsCarousel,
-    Career,
-    Clients,
-    Newsletter,
-    Footer,
-  },
 };
 </script>
