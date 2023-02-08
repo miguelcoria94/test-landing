f387a3ccb13d4e1cd04353db44c5b9511fa52384
miguelcoria94 <miguelcoria94@gmail.com>
2023-02-08 16:17:55 -0600

    added the blog layout

--- src/partials/WidgetBook.vue
+++ src/partials/WidgetBook.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="rounded-lg border bg-slate-200 border-slate-300 odd:rotate-1 even:-rotate-1 p-5">
+  <div class="rounded-lg border border-slate-200 odd:rotate-1 even:-rotate-1 p-5">
     <div class="font-aspekta font-[650] text-center mb-3">Download Case Study</div>
     <div class="text-center">
       <img class="inline-flex rounded-lg shadow-lg rotate-3" src="../images/book.png" width="148" height="190" alt="Book" />
