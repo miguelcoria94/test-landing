018d6710048fdb588dfe5e62702c7e667cad5468
miguelcoria94 <miguelcoria94@gmail.com>
2023-02-08 16:15:00 -0600

    added the blog layout

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -5,12 +5,39 @@
     <Header />
 
     <!-- Page content -->
-    <main class="grow">
+    <main class="w-full h-full">
 
       
       <!-- Blog Header -->
       <BlogHero />
-      <BlogHeader />
+      <!-- <BlogHeader /> -->
+      <!-- Content -->
+          <div class="grow md:flex space-y-8 md:space-y-0 md:space-x-8 pb-16 md:pb-20 p-20">
+
+            <!-- Middle area -->
+            <div class="grow">
+              <div class="max-w-[800px]">
+                <div class="space-y-10">
+
+                  <ArticlesList />
+                  <Talks />
+
+                </div>
+              </div>
+            </div>
+
+            <!-- Right sidebar -->
+            <aside class="md:w-[240px] lg:w-[300px] shrink-0">
+              <div class="space-y-6">
+
+                <WidgetNewsletter />
+                <!-- <WidgetSponsor /> -->
+                <WidgetBook />
+
+              </div>
+            </aside>
+
+          </div>
       
 
     </main>
@@ -26,6 +53,12 @@ import Header from './../partials/Header.vue'
 import Footer from './../partials/Footer.vue'
 import BlogHero from './../partials/BlogHero.vue'
 import BlogHeader from './../partials/BlogHeader.vue'
+import ArticlesList from "./../partials/ArticlesList.vue"
+import Talks from './../partials/Talks.vue'
+import Projects from './../partials/Projects.vue'
+import WidgetNewsletter from '../partials/WidgetNewsletter.vue'
+import WidgetSponsor from '../partials/WidgetSponsor.vue'
+import WidgetBook from '../partials/WidgetBook.vue'
 
 export default {
   name: 'Blog',
@@ -33,7 +66,13 @@ export default {
     Header,
     Footer,
     BlogHero,
-    BlogHeader
+    BlogHeader,
+    ArticlesList,
+    Talks,
+    Projects,
+    WidgetNewsletter,
+    WidgetSponsor,
+    WidgetBook
   },
 };
 </script>
---
+++ src/partials/WidgetBook.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="rounded-lg border bg-slate-200 border-slate-300 odd:rotate-1 even:-rotate-1 p-5">
+    <div class="font-aspekta font-[650] text-center mb-3">Download Case Study</div>
+    <div class="text-center">
+      <img class="inline-flex rounded-lg shadow-lg rotate-3" src="../images/book.png" width="148" height="190" alt="Book" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WidgetBook',
+}
+</script>
