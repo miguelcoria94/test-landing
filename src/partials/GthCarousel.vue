c83267ef3b61ff37afe83d47c1bd69809e0f0855
miguelcoria94 <miguelcoria94@gmail.com>
2023-02-22 19:48:32 -0600

    finished case studies

--- src/partials/GthCarousel.vue
+++ src/partials/GthCarousel.vue
@@ -1,12 +1,12 @@
 <template>
   <section class="bg-green-500">
 
-    <div class="max-w-6xl mx-auto px-4 sm:px-6">
+    <div class="max-w-6xl mx-auto px-4 sm:px-6 mt-20">
       <div class="py-12 md:py-20">
 
         <!-- Section header -->
         <div class="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-          <h2 class="h2 font-cabinet-grotesk text-gray-900">Trending collections</h2>
+          <h2 class="h2 font-cabinet-grotesk text-gray-900">Trending Case Studies</h2>
         </div>
 
         <!-- Carousel built with Swiper.js [https://swiperjs.com/] -->
@@ -16,159 +16,53 @@
             <!-- Carousel items -->
             <div class="swiper-slide h-auto flex flex-col">
               <!-- Image -->
-              <img class="w-full aspect-[7/4] object-cover" src="../images/carousel-01.jpg" width="259" height="148" alt="Carousel 01" />
+              <img class="w-full aspect-[7/4] object-cover" src="../images/bhhs-case-study.png" width="259" height="148" alt="Carousel 01" />
               <!-- White box -->
               <div class="grow bg-white px-4 pb-6">
-                <!-- Avatars -->
-                <div class="flex items-start -space-x-3 -ml-0.5 mb-4 -mt-5">
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-01.jpg" width="36" height="36" alt="Avatar 01" />
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-02.jpg" width="36" height="36" alt="Avatar 02" />
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-03.jpg" width="36" height="36" alt="Avatar 03" />
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-04.jpg" width="36" height="36" alt="Avatar 04" />
-                </div>
+               
                 <!-- Title -->
-                <a class="inline-block font-cabinet-grotesk font-bold text-xl decoration-blue-500 decoration-2 underline-offset-2 hover:underline" href="#0">Digital Art</a>
-                <div class="text-sm text-gray-500 italic">34 collections</div>
+                <a class="inline-block font-cabinet-grotesk font-bold text-xl decoration-blue-500 decoration-2 underline-offset-2 hover:underline" href="#0">Beverly Hills Unified School District</a>
+                <div class="text-sm text-gray-500 italic">Case Study</div>
               </div>
             </div>
             <div class="swiper-slide h-auto flex flex-col">
               <!-- Image -->
-              <img class="w-full aspect-[7/4] object-cover" src="../images/carousel-02.jpg" width="259" height="148" alt="Carousel 02" />
+              <img class="w-full aspect-[7/4] object-cover" src="../images/laguna.png" width="259" height="148" alt="Carousel 02" />
               <!-- White box -->
               <div class="grow bg-white px-4 pb-6">
-                <!-- Avatars -->
-                <div class="flex items-start -space-x-3 -ml-0.5 mb-4 -mt-5">
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-05.jpg" width="36" height="36" alt="Avatar 05" />
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-06.jpg" width="36" height="36" alt="Avatar 06" />
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-07.jpg" width="36" height="36" alt="Avatar 07" />
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-08.jpg" width="36" height="36" alt="Avatar 08" />
-                </div>
+               
                 <!-- Title -->
-                <a class="inline-block font-cabinet-grotesk font-bold text-xl decoration-blue-500 decoration-2 underline-offset-2 hover:underline" href="#0">Gradients</a>
-                <div class="text-sm text-gray-500 italic">129 collections</div>
+                <a class="inline-block font-cabinet-grotesk font-bold text-xl decoration-blue-500 decoration-2 underline-offset-2 hover:underline" href="#0">Laguna Beach Counseling</a>
+                <div class="text-sm text-gray-500 italic">Coming Soon</div>
               </div>
             </div>
             <div class="swiper-slide h-auto flex flex-col">
               <!-- Image -->
-              <img class="w-full aspect-[7/4] object-cover" src="../images/carousel-03.jpg" width="259" height="148" alt="Carousel 03" />
+              <img class="w-full aspect-[7/4] object-cover" src="../images/greenwood.png" width="259" height="148" alt="Carousel 03" />
               <!-- White box -->
               <div class="grow bg-white px-4 pb-6">
-                <!-- Avatars -->
-                <div class="flex items-start -space-x-3 -ml-0.5 mb-4 -mt-5">
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-09.jpg" width="36" height="36" alt="Avatar 09" />
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-08.jpg" width="36" height="36" alt="Avatar 08" />
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-10.jpg" width="36" height="36" alt="Avatar 10" />
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-11.jpg" width="36" height="36" alt="Avatar 11" />
-                </div>
+                
                 <!-- Title -->
-                <a class="inline-block font-cabinet-grotesk font-bold text-xl decoration-blue-500 decoration-2 underline-offset-2 hover:underline" href="#0">Liquid 3D</a>
-                <div class="text-sm text-gray-500 italic">49 collections</div>
+                <a class="inline-block font-cabinet-grotesk font-bold text-xl decoration-blue-500 decoration-2 underline-offset-2 hover:underline" href="#0">Greenwood College </a>
+                <div class="text-sm text-gray-500 italic">Coming Soon</div>
               </div>
             </div>
+            
             <div class="swiper-slide h-auto flex flex-col">
               <!-- Image -->
-              <img class="w-full aspect-[7/4] object-cover" src="../images/carousel-04.jpg" width="259" height="148" alt="Carousel 04" />
+              <img class="w-full aspect-[7/4] object-cover" src="../images/helmet.png" width="259" height="148" alt="Carousel 08" />
               <!-- White box -->
               <div class="grow bg-white px-4 pb-6">
-                <!-- Avatars -->
-                <div class="flex items-start -space-x-3 -ml-0.5 mb-4 -mt-5">
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-18.jpg" width="36" height="36" alt="Avatar 18" />
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-08.jpg" width="36" height="36" alt="Avatar 08" />
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-01.jpg" width="36" height="36" alt="Avatar 01" />
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-11.jpg" width="36" height="36" alt="Avatar 11" />
-                </div>
+                
                 <!-- Title -->
-                <a class="inline-block font-cabinet-grotesk font-bold text-xl decoration-blue-500 decoration-2 underline-offset-2 hover:underline" href="#0">Abstraction</a>
-                <div class="text-sm text-gray-500 italic">24 collections</div>
-              </div>
-            </div>
-            <div class="swiper-slide h-auto flex flex-col">
-              <!-- Image -->
-              <img class="w-full aspect-[7/4] object-cover" src="../images/carousel-05.jpg" width="259" height="148" alt="Carousel 05" />
-              <!-- White box -->
-              <div class="grow bg-white px-4 pb-6">
-                <!-- Avatars -->
-                <div class="flex items-start -space-x-3 -ml-0.5 mb-4 -mt-5">
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-09.jpg" width="36" height="36" alt="Avatar 09" />
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-12.jpg" width="36" height="36" alt="Avatar 12" />
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-13.jpg" width="36" height="36" alt="Avatar 13" />
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-14.jpg" width="36" height="36" alt="Avatar 14" />
-                </div>
-                <!-- Title -->
-                <a class="inline-block font-cabinet-grotesk font-bold text-xl decoration-blue-500 decoration-2 underline-offset-2 hover:underline" href="#0">Landscapes</a>
-                <div class="text-sm text-gray-500 italic">27 collections</div>
-              </div>
-            </div>
-            <div class="swiper-slide h-auto flex flex-col">
-              <!-- Image -->
-              <img class="w-full aspect-[7/4] object-cover" src="../images/carousel-06.jpg" width="259" height="148" alt="Carousel 06" />
-              <!-- White box -->
-              <div class="grow bg-white px-4 pb-6">
-                <!-- Avatars -->
-                <div class="flex items-start -space-x-3 -ml-0.5 mb-4 -mt-5">
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-15.jpg" width="36" height="36" alt="Avatar 01" />
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-11.jpg" width="36" height="36" alt="Avatar 11" />
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-16.jpg" width="36" height="36" alt="Avatar 16" />
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-17.jpg" width="36" height="36" alt="Avatar 17" />
-                </div>
-                <!-- Title -->
-                <a class="inline-block font-cabinet-grotesk font-bold text-xl decoration-blue-500 decoration-2 underline-offset-2 hover:underline" href="#0">Pastel</a>
-                <div class="text-sm text-gray-500 italic">22 collections</div>
-              </div>
-            </div>
-            <div class="swiper-slide h-auto flex flex-col">
-              <!-- Image -->
-              <img class="w-full aspect-[7/4] object-cover" src="../images/carousel-07.jpg" width="259" height="148" alt="Carousel 07" />
-              <!-- White box -->
-              <div class="grow bg-white px-4 pb-6">
-                <!-- Avatars -->
-                <div class="flex items-start -space-x-3 -ml-0.5 mb-4 -mt-5">
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-09.jpg" width="36" height="36" alt="Avatar 09" />
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-18.jpg" width="36" height="36" alt="Avatar 18" />
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-10.jpg" width="36" height="36" alt="Avatar 10" />
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-05.jpg" width="36" height="36" alt="Avatar 05" />
-                </div>
-                <!-- Title -->
-                <a class="inline-block font-cabinet-grotesk font-bold text-xl decoration-blue-500 decoration-2 underline-offset-2 hover:underline" href="#0">Dark 3D</a>
-                <div class="text-sm text-gray-500 italic">112 collections</div>
-              </div>
-            </div>
-            <div class="swiper-slide h-auto flex flex-col">
-              <!-- Image -->
-              <img class="w-full aspect-[7/4] object-cover" src="../images/carousel-08.jpg" width="259" height="148" alt="Carousel 08" />
-              <!-- White box -->
-              <div class="grow bg-white px-4 pb-6">
-                <!-- Avatars -->
-                <div class="flex items-start -space-x-3 -ml-0.5 mb-4 -mt-5">
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-06.jpg" width="36" height="36" alt="Avatar 06" />
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-05.jpg" width="36" height="36" alt="Avatar 05" />
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-08.jpg" width="36" height="36" alt="Avatar 08" />
-                  <img class="rounded-full border-2 border-white box-content" src="../images/carousel-avatar-07.jpg" width="36" height="36" alt="Avatar 07" />
-                </div>
-                <!-- Title -->
-                <a class="inline-block font-cabinet-grotesk font-bold text-xl decoration-blue-500 decoration-2 underline-offset-2 hover:underline" href="#0">Baroque</a>
-                <div class="text-sm text-gray-500 italic">77 collections</div>
+                <a class="inline-block font-cabinet-grotesk font-bold text-xl decoration-blue-500 decoration-2 underline-offset-2 hover:underline" href="#0">Hemet Unified School District </a>
+                <div class="text-sm text-gray-500 italic">Coming Soon</div>
               </div>
             </div>
           </div>
         </div>
 
-        <!-- Arrows -->
-        <div class="flex mt-12 space-x-4 justify-end">
-          <button class="carousel-prev relative z-20 w-14 h-14 rounded-full flex items-center justify-center group bg-gray-700 hover:bg-blue-500 transition duration-150 ease-in-out">
-            <span class="sr-only">Previous</span>
-            <svg class="w-4 h-4 fill-gray-400 group-hover:fill-white transition duration-150 ease-in-out" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-              <path d="M6.7 14.7l1.4-1.4L3.8 9H16V7H3.8l4.3-4.3-1.4-1.4L0 8z" />
-            </svg>
-          </button>
-          <button class="carousel-next relative z-20 w-14 h-14 rounded-full flex items-center justify-center group bg-gray-700 hover:bg-blue-500 transition duration-150 ease-in-out">
-            <span class="sr-only">Next</span>
-            <svg class="w-4 h-4 fill-gray-400 group-hover:fill-white transition duration-150 ease-in-out" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-              <path d="M9.3 14.7l-1.4-1.4L12.2 9H0V7h12.2L7.9 2.7l1.4-1.4L16 8z" />
-            </svg>
-          </button>
-        </div>
-
+       
       </div>
     </div>
   </section>
